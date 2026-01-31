6a1fa957a37892aed29c444bcd6863a567096a31
Nithish-k <nithish.k123user@gmail.com>
2026-01-31 06:07:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // Allow frontend
+    origin: ['http://localhost:5173', 'http://localhost:5174','https://anime-p-frontend.vercel.app/'],
+    // Allow frontend
     credentials: true
 }));
 app.use(cookieParser());
