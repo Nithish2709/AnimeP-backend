64ac2352a3dc0598e6a588b6b76b707ac3d090ee
Nithish-k <nithish.k123user@gmail.com>
2026-01-31 09:00:00 +0530

    Remove duplicate URL from CORS origin list

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174','https://anime-p-frontend.vercel.app/','https://anime-p-frontend.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:5174','https://anime-p-frontend.vercel.app'],
     // Allow frontend
     credentials: true
 }));
